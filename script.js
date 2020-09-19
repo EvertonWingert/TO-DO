2309a1a63070338ea409b21cb3beb67a9b67734e
EvertonWingert <54149497+EvertonWingert@users.noreply.github.com>
2020-09-19 11:20:17 -0300

    mudança no visual
    novas funções
    novas cores

--- script.js
+++ script.js
@@ -2,67 +2,53 @@ const inputEl = document.getElementById("input-todo");
 const btnAddEl = document.getElementById("create");
 var todolistEl = document.getElementById("list-group");
 const todoEmpty = `<div><p class="todo-empty">Sem to-do</p></div>`
+const restanteEL = document.getElementById("total");
 todolistEl.innerHTML = todoEmpty;
 var todoList = [];
 
 
-const geraId = () => {
-    return Math.random() *100;
-
+function todoFactory(conteudo){
+    let id = (Math.random() *100);
+    return {
+        id,
+        conteudo
+    }
 }
-
 function createTodo(){
     if(inputEl.value == '') return
-    let todo = {
-        id: geraId(),
-        conteudo: inputEl.value
-    }
-    todoList.push(todo);
+    todoList.push(todoFactory(inputEl.value));
     inputEl.value = '';
     renderTodo();
 }
 function deleteTodo(key) {
-    
-    console.log(key)
-    
-    todoList.forEach((conteudo,i) => {
-        if(conteudo.id == key){
-            todoList.splice(i,1);
-        }
-    })
-
+    let index = todoList.findIndex(function(item) {
+        return item.id === key;
+    });
+    todoList.splice(index,1);
     renderTodo();
 }
-function updateTodo(newValue,key){  
-    todoList.forEach((conteudo) => {
-        if(conteudo.id == key){
-            conteudo.conteudo = newValue.innerText;
-        }
-    })
+function updateTodo(novoValor,key){ 
+    let index = todoList.findIndex(function(item) {
+        return item.id === key;
+    });
+    todoList[index].conteudo = novoValor.innerHTML;
 }
+
 function renderTodo(){
     if(todoList.length == 0){
         todolistEl.innerHTML = todoEmpty;
-        
+        restanteEL.innerHTML = 0;
     }else { 
         var list = "";
         todoList.forEach((conteudo) => {
-            list +=  `<li key="${conteudo.id}" class="todoItem">
-                        <a contentEditable="true" onBlur="updateTodo(this,key)" id="conteudo">${conteudo.conteudo}</a>
-                        <button onClick="deleteTodo(${conteudo.id})"> Deletar </button>   
-                    </li>
+            
+            list +=  `<li key="${conteudo.id}" class="todoItem" id="todo">
+                        <a contentEditable="true" onBlur="updateTodo(this,${conteudo.id})" id="conteudo">${conteudo.conteudo}</a>
+                        <button onClick="deleteTodo(${conteudo.id})" id="deleteTodo"> Deletar </button>   
+                      </li>
                         `
-                
         })
         todolistEl.innerHTML = list;
-    }    
-    
+        restanteEL.innerHTML = todoList.length; 
+    } 
 }
-
-/*
-for(let i = 0; i < 30; i++){
-    let todo = {id:geraId(), conteudo:i }
-    todoList.push(todo);
-}
-renderTodo();
-*/
