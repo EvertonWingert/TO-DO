4b8e6d19e06c1f51c7faa443bec467a81c4ba8d3
EvertonWingert <54149497+EvertonWingert@users.noreply.github.com>
2020-09-28 16:22:07 -0300

    Print da tela

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 import { getLocalStorage } from "./localStorage.js";
 import { createTodo,deleteAllTodos,renderTodo } from "./todo.js";
-import {firebaseConfig} from "./db/firebaseConfig";
 
 const todoFormElement = document.getElementById("form");
 const deleteALLElement = document.getElementById("delete-all");
-
-firebase.initializeApp(firebaseConfig);
 
 var todoList = getLocalStorage();
 renderTodo();
